eb4891062f76a6d985adfe71f73e985c087552bb
Jeff Parent <jeff.parent@gmail.com>
2010-12-19 15:12:18 -0600

    Added basic makefile templates

---
+++ config.mk
@@ -0,0 +1,12 @@
+# Customize to fit your system
+
+# paths 
+PREFIX			= /usr/local/goblin
+MANPREFIX		= ${PREFIX}/share/man
+
+OBJTYPE			= ${GOARCH}
+O						= 8
+
+# Compilers
+GC					= ${O}g
+LD					= ${O}l
