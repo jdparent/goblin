7e1f7989ba080b3640fde7fa7c3a1d441f57f03d
Jeff Parent <jeff.parent@gmail.com>
2010-12-19 16:33:49 -0600

    Created command: cat

---
+++ cat/cat.go
@@ -0,0 +1,62 @@
+// cat - catenate files
+//
+// Code is pretty much line for line with the golang.org website's example 
+// rotting cats.  Not a while lot can be changed to get a more simple 
+// implementation.
+package main
+
+import (
+	"os"
+	"flag"
+	"fmt"
+)
+
+func cat(f *os.File) {
+	const NBUF = 8192
+	var buf [NBUF]byte
+
+	for {
+		switch nr, rerr := f.Read(buf[:]); true {
+		case nr > 0:
+			var nw, werr = os.Stdout.Write(buf[0:nr])
+			if nw != nr {
+				fmt.Fprintf(
+					os.Stderr,
+					"cat: write error copying %s: %s",
+					f.Name(),
+					werr.String())
+				os.Exit(1)
+			}
+		case nr == 0:
+			return
+		case nr < 0:
+			fmt.Fprintf(
+				os.Stderr,
+				"cat: error reading %s: %s",
+				f.Name(),
+				rerr.String())
+			os.Exit(1)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() > 0 {
+		for i := 0; i < flag.NArg(); i++ {
+			var f, err = os.Open(flag.Arg(i), os.O_RDONLY, 0666)
+			if f == nil {
+				fmt.Fprintf(
+					os.Stderr,
+					"cat: can't open %s: %s",
+					flag.Arg(i),
+					err.String())
+				os.Exit(1)
+			}
+			cat(f)
+			f.Close()
+		}
+	} else {
+		cat(os.Stdin)
+	}
+}
