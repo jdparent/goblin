9177874613a3817f1e8a5078f7d9af8153a5881b
Robert Bloomquist <bloomquist.robert@gmail.com>
2012-04-08 22:37:16 -0400

    updated basename to go1 and simplified.

--- basename/basename.go
+++ basename/basename.go
@@ -7,50 +7,32 @@ import (
 	"flag"
 )
 
-var dirMode = flag.Bool("d", false, "Print directory component")
+var dflag = flag.Bool("d", false, "Print directory component")
 
 func usage() {
-	fmt.Fprintf(
-		os.Stderr,
-		"usage: basename [-d] string [suffix]\n")
+	fmt.Fprintln(os.Stderr, "usage: basename [-d] string [suffix]")
 	os.Exit(1)
 }
 
 func printDir(str string) {
-	index := strings.LastIndex(str, "/")
-
-	if index == -1 {
-		fmt.Fprintf(os.Stdout, ".\n")
-	} else {
-		if index == 0 {
-			fmt.Fprintf(os.Stdout, "\n")
-		} else {
-			fmt.Fprintf(os.Stdout, "%s\n", str[0:index])
-		}
+	i := strings.LastIndex(str, "/")
+	if i < 0 {
+		fmt.Println(".")
+		return
 	}
+	fmt.Println(str[:i])
 }
 
 func printName(str, suf string) {
-	index := strings.LastIndex(str, "/")
-	name := str[index+1:len(str)]
-
-	if len(suf) > 0 {
-		index2 := strings.LastIndex(name, suf)
-
-		if index2 != -1 {
-			name = name[0:index2]
-		}
-	}
-	fmt.Fprintf(os.Stdout, "%s\n", name)
+	i, j := strings.LastIndex(str, "/"), strings.LastIndex(str, suf)
+	fmt.Println(str[i+1:j])
 }
 
 func main() {
 	flag.Parse()
-
 	if flag.NArg() == 0 || flag.NArg() > 2 {
 		usage()
 	}
-
 	if *dirMode {
 		printDir(flag.Arg(0))
 	} else {
