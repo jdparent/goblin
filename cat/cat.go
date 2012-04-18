8d2cd5d4671da443e54fc50d2fa83bc7c36a7053
Robert Bloomquist <rbloomy6@me.com>
2012-04-18 02:24:58 -0300

    Error messages from os.Open, file.Read, and file.Write already return context, so no need to add it again.

--- cat/cat.go
+++ cat/cat.go
@@ -22,13 +22,13 @@ func cat(f *os.File) {
 		case nr > 0:
 			nw, werr := os.Stdout.Write(b[0:nr])
 			if nw != nr {
-				fmt.Fprintf(os.Stderr, "cat: write error copying %s: %s", f.Name(), werr.Error())
+				fmt.Fprintln(os.Stderr, "cat:", werr.Error())
 				os.Exit(1)
 			}
 		case nr == 0:
 			return
 		case nr < 0:
-			fmt.Fprintf(os.Stderr, "cat: error reading %s: %s", f.Name(), rerr.Error())
+			fmt.Fprintln(os.Stderr, "cat:", rerr.Error())
 			os.Exit(1)
 		}
 	}
@@ -41,8 +41,8 @@ func main() {
 	} else {
 		for _, v := range flag.Args() {
 			f, err := os.Open(v)
-			if f == nil {
-				fmt.Fprintf(os.Stderr, "cat: can't open %s: %s", v, err.Error())
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "cat:" err.Error())
 				os.Exit(1)
 			}
 			cat(f)
